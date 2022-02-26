d25ac2d6dcdc3a2aa392fd2a32ca6010fc0a9113
wqking <wqking@outlook.com>
2022-02-26 20:38:23 +0800

    Detect C++17 correctly in MSVC

--- include/metapp/compiler.h
+++ include/metapp/compiler.h
@@ -1,16 +1,6 @@
 #ifndef COMPILER_H_969872685611
 #define COMPILER_H_969872685611
 
-#if __cplusplus >= 201703L
-	#define METAPP_SUPPORT_STANDARD_17
-	#define METAPP_NOEXCEPT_BELONGS_TO_FUNCTION_TYPE
-#else
-	// Visual Studio 2017 RTW (15.0)
-	#if defined(_MSC_VER) && _MSC_VER >= 1910
-		#define METAPP_SUPPORT_STANDARD_17
-	#endif
-#endif
-
 #if defined(_MSC_VER)
 #define METAPP_COMPILER_VC
 #elif defined(__clang__)
@@ -21,6 +11,18 @@
 #define METAPP_COMPILER_UNKNOWN
 #endif
 
+#if __cplusplus >= 201703L
+	#define METAPP_SUPPORT_STANDARD_17
+	#define METAPP_NOEXCEPT_BELONGS_TO_FUNCTION_TYPE
+#else
+	// Visual Studio 2017 RTW (15.0)
+	#if defined(_MSC_VER) && _MSC_VER >= 1910
+		// the code is inspired by vcruntime.h
+		#if defined(_MSVC_LANG) && (_MSVC_LANG > 201402L)
+			#define METAPP_SUPPORT_STANDARD_17
+		#endif
+	#endif
+#endif
 
 
 #endif
