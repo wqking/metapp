20d235a230e3f68f7a4db5fafc5302ac68eda225
wqking <wqking@outlook.com>
2022-02-27 20:27:33 +0800

    Removed getMetaAccessible from reference meta type

--- tests/unittest/metatypes/test_reference.cpp
+++ tests/unittest/metatypes/test_reference.cpp
@@ -16,19 +16,3 @@ TEST_CASE("metatypes, int &") {
 	REQUIRE(matchUpTypeKinds(v.getMetaType(), { tkReference, tkInt }));
 }
 
-TEST_CASE("metatypes, int &, accessible")
-{
-	int n = 5;
-	metapp::Variant v;
-	v.set<int &>(n);
-	REQUIRE(v.getMetaType()->getMetaAccessible()->get(v, nullptr).get<int>() == 5);
-
-	v.getMetaType()->getMetaAccessible()->set(v, nullptr, 38);
-	REQUIRE(n == 38);
-	REQUIRE(v.getMetaType()->getMetaAccessible()->get(v, nullptr).get<int>() == 38);
-
-	v.getMetaType()->getMetaAccessible()->set(v, nullptr, 98.0);
-	REQUIRE(n == 98);
-	REQUIRE(v.getMetaType()->getMetaAccessible()->get(v, nullptr).get<int>() == 98);
-}
-
