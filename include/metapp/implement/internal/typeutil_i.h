6dbd5a5dc2ec1f827f5be14428dead4062cbe43c
wqking <wqking@outlook.com>
2022-03-28 20:48:45 +0800

    Fixed a bug in IO streaming operator detecting type traits

--- include/metapp/implement/internal/typeutil_i.h
+++ include/metapp/implement/internal/typeutil_i.h
@@ -23,6 +23,35 @@
 #include <istream>
 #include <ostream>
 
+namespace metapp_top_internal_ {
+
+// In HasInputStreamOperator_ and HasOutputStreamOperator_, we have to use std::declval<std::istream &>()
+// instead of std::declval<std::istream>(), because C++11 adds a generic function that accepts
+// "std::istream &&" even if the right value doesn't apply to the << operator.
+// See https://stackoverflow.com/questions/37480778/declval-expression-for-sfinae-with-stdostream
+template <typename T>
+struct HasInputStreamOperator_
+{
+	template <typename C> static std::true_type test(
+		typename std::remove_reference<decltype(std::declval<std::istream &>() >> std::declval<C &>())>::type *);
+	template <typename C> static std::false_type test(...);
+
+	enum { value = !! decltype(test<T>(0))() };
+};
+
+template <typename T>
+struct HasOutputStreamOperator_
+{
+	template <typename C> static std::true_type test(
+		typename std::remove_reference<decltype(std::declval<std::ostream &>() << std::declval<C>())>::type *);
+	template <typename C> static std::false_type test(...);
+
+	enum { value = !! decltype(test<T>(0))() };
+};
+
+
+} // namespace metapp_top_internal_
+
 namespace metapp {
 
 namespace internal_ {
@@ -82,29 +111,10 @@ struct VariantReturnType
 	>::type;
 };
 
-// In HasInputStreamOperator and HasOutputStreamOperator, we have to use std::declval<std::istream &>()
-// instead of std::declval<std::istream>(), because C++11 adds a generic function that accepts
-// "std::istream &&" even if the right value doesn't apply to the << operator.
-// See https://stackoverflow.com/questions/37480778/declval-expression-for-sfinae-with-stdostream
 template <typename T>
-struct HasInputStreamOperator
-{
-	template <typename C> static std::true_type test(
-		typename std::remove_reference<decltype(std::declval<std::istream &>() >> std::declval<C &>())>::type *);
-	template <typename C> static std::false_type test(...);
-
-	enum { value = !! decltype(test<T>(0))() };
-};
-
+using HasInputStreamOperator = metapp_top_internal_::HasInputStreamOperator_<T>;
 template <typename T>
-struct HasOutputStreamOperator
-{
-	template <typename C> static std::true_type test(
-		typename std::remove_reference<decltype(std::declval<std::ostream &>() << std::declval<C>())>::type *);
-	template <typename C> static std::false_type test(...);
-
-	enum { value = !! decltype(test<T>(0))() };
-};
+using HasOutputStreamOperator = metapp_top_internal_::HasOutputStreamOperator_<T>;
 
 #define METAPP_HAS_MEMBER(member) \
 	template <typename T> struct HasMember_ ## member { \
