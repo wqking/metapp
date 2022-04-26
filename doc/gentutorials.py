521466292b9f8435ada185633b00eebe6420a045
wqking <wqking@outlook.com>
2022-04-26 16:10:35 +0800

    Improved gentutorials.py

--- doc/gentutorials.py
+++ doc/gentutorials.py
@@ -18,7 +18,8 @@ def makeFileInfo(fileName) :
 	}
 
 fileInfoList = [
-	makeFileInfo('tutorial_callable')
+	makeFileInfo('tutorial_callable'),
+	makeFileInfo('tutorial_metaclass'),
 ]
 
 def writeFile(fileName, content) :
@@ -29,10 +30,41 @@ def readFile(fileName) :
 	with codecs.open(fileName, "r", "utf-8") as file :
 		return file.read()
 
+def doTabToSpace(text) :
+	def callback(m) :
+		return '    ' * len(m.group(1))
+	text = re.sub(r'^(\t+)', callback, text)
+	return text
+
+def doProcessCodeLines(lineList, beginIndex, endIndex) :
+	# find out the redundant leading spaces to remove from (unindent)
+	leadingSpaceLength = None
+	index = beginIndex
+	while index <= endIndex :
+		line = lineList[index]
+		matches = re.match(r'^(\s*)\S+', line)
+		if matches is not None :
+			leadingLength = len(matches.group(1))
+			if (leadingSpaceLength is None) or (leadingLength < leadingSpaceLength) :
+				leadingSpaceLength = leadingLength
+		index += 1
+
+	if leadingSpaceLength is None :
+		leadingSpaceLength = 0
+	index = beginIndex
+	while index <= endIndex :
+		line = lineList[index]
+		if leadingSpaceLength > 0 :
+			line = line[leadingSpaceLength:]
+		line = doTabToSpace(line)
+		lineList[index] = line
+		index += 1
+
 def doProcessFile(inputFile, outputFile) :
 	lineList = readFile(inputFile).splitlines()
 	outputLineList = []
 	state = State.none
+	codeBeginIndex = 0
 	for line in lineList :
 		matches = re.match(r'\s*[\/\*]*\s*(\w+)\s*[\/\*]*\s*', line)
 		command = None
@@ -48,12 +80,14 @@ def doProcessFile(inputFile, outputFile) :
 		elif command == 'code' :
 			if state == State.code :
 				state = State.none
+				doProcessCodeLines(outputLineList, codeBeginIndex, len(outputLineList) - 1)
 				outputLineList.append("```")
 				outputLineList.append("")
 			else :
 				state = State.code
 				outputLineList.append("")
 				outputLineList.append("```c++")
+				codeBeginIndex = len(outputLineList)
 		else :
 			if state != State.none :
 				outputLineList.append(line)
